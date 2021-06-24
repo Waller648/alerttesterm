194ab18680f84d9d4cdb249a156f4daa271bb594
Waller648 <77145774+Waller648@users.noreply.github.com>
2021-06-24 21:09:58 +0300

    e
    e

--- alert.js
+++ alert.js
@@ -1,5 +1,5 @@
 le._dom.taskbar.innerHTML = "Complete the alert setup"
 le._dom.background.style.background = "darkblue"
 $el.each(".ui_icon",function(a){a.classList.add("hide")})
-var i = $window({html
+var i = $window({html:"hi kid"})
 i.el.base.style.padding="1px 1px 1px 1px",i.el.base.style.boxShadow="rgb(0, 0, 0) 1px 0px 0px 0px, rgb(0, 0, 0) 0px 1px 0px 0px, rgb(0, 0, 0) 1px 1px 0px 0px, rgb(255, 255, 255) 1px 1px 0px 0px inset, rgba(0, 0, 0, 0.5) 0px 0px 1px 1px",i.el.header.style.background="linear-gradient(90deg, #0A246A 0, #A6CAF0 100%)",i.el.base.classList.add("splash"),i.el.body.classList.add("splash"),i.el.footer.classList.add("splash"),i.el.header.classList.add("splash"),i.el.title.classList.add("splash")
